8d3caf7172c0af4bc87cc40098fc090f375f689b
Alexeiev Araujo <ceievfa@gmail.com>
2024-06-11 13:49:35 +0100

    add news variables for cpu, disk and more

--- variables.tf
+++ variables.tf
@@ -51,14 +51,32 @@ variable "site" {
   description = "URL to Proxmox"
 }
 
-variable "memory" {
+variable "vm_memory" {
   type = number
   description = "Memory in MB"
   default = 1024
 }
 
-variable "cores" {
+variable "vm_cpu" {
   type = number
-  description = "CPU cores for VM"
+  description = "Number of processors of the VM"
   default = 1
+}
+
+variable "vm_disk" {
+  type = number
+  description = "Disk in GB"
+  default = "30"
+}
+
+variable "net" {
+  type = string
+  description = "Interface name"
+  default = "vmbr0"
+}
+
+variable "net_vlan" {
+  type = number
+  description = "Vlan Tag for segmented network"
+  default = -1
 }
